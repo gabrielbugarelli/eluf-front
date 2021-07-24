9be77832f06255d9e2870a5c11da52911d5004ba
Gabriel Fonseca <gabrielfonsecaoficial@gmail.com>
2021-07-24 11:04:21 -0400

    feat(routes): configuration of routes on the user page

---
+++ src/components/UserData/UserData.jsx
@@ -0,0 +1,11 @@
+import Layout from "../Layout/Layout";
+
+const UserData = () => {
+  return (
+    <Layout>
+      USER DATA COMPONENT
+    </Layout>
+  );
+}
+
+export default UserData;
---
+++ src/components/UserDataLocation/UserDataLocation.jsx
@@ -0,0 +1,11 @@
+import Layout from "../Layout/Layout";
+
+const UserDataLocation = () => {
+  return (
+    <Layout>
+      user data location
+    </Layout>
+  );
+}
+
+export default UserDataLocation;
