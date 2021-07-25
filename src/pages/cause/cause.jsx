7dd5e8ffccca58b0619143f752d5f64ed57cd556
Gabriel Fonseca <gabrielfonsecaoficial@gmail.com>
2021-07-24 20:11:01 -0400

    feat: cause page implementation

--- src/pages/cause/cause.jsx
+++ src/pages/cause/cause.jsx
@@ -1,9 +1,46 @@
+//components
 import Layout from '../../components/Layout/Layout';
+import { Container, Title, Context, Interaction, Icons } from './s_cause';
+
+//icons
+import { AiFillLike, AiFillDislike } from "react-icons/ai";
 
 const cause = () => {
   return (
     <Layout>
-      Causa do projeto
+      <Container>
+        <section>
+          <Title>Causa do Projeto</Title>
+        </section>
+
+        <Context>
+          <p style={{textIndent:'30px'}}>Você Sabia que cerca de 17% dos alimentos disponíveis no mundo acabam sendo jogados no lixo?</p>
+
+          <p style={{textIndent:'30px'}}>Segundo a ONU, estima-se que entre 8% e 10% das emissões globais de gases de efeito estufa estão associadas a alimentos que não são consumidos.</p>
+
+          <p style={{textIndent:'30px'}}>A plataforma Eluf veio para contribuir, melhorando a alimentação em muitas mesas e para ajudar na redução do desperdício de alimentos. </p>  
+
+          <p style={{textIndent:'30px'}}>Além de diminuir perdas, a plataforma trás uma simples forma de comercialização. Ela serve para o pequeno produtor, para o comercio e para todos aqueles que desejam comercializar de forma fácil e consciente.</p>
+
+          <p style={{textIndent:'30px'}}>Funciona dessa maneira, a pessoa abre o aplicativo na pagina home, na qual vai estar listada os produtos, pagina que carrinho onde estara todos os alimentos selecionados, pagina maps vai simular a experiência do usuário, tera a pagina do usuario onde armazenara seus dados </p>
+
+          <p style={{textIndent:'30px'}}>A Eluf, além de ser uma plataforma simples e intuitiva ela chega no mercado para reduzir o desperdício, a fome e ajudar
+            a promover um mundo mais sustentável, nos tornando pessoas mais conscientes, se alinhando com os objetivos de desenvolvimento sustentável(ODS) que faz parte da plataforma agenda 2030.</p>
+        </Context>
+
+        <Interaction>
+          Essa informação foi útil?
+          <Icons>
+              <AiFillLike 
+                className="hover:text-green-500" 
+              />
+
+              <AiFillDislike 
+                className="hover:text-red-500"
+              />
+          </Icons>
+        </Interaction>
+      </Container>
     </Layout>
   );
 }
---
+++ src/pages/cause/s_cause.jsx
@@ -0,0 +1,38 @@
+import tw from "tailwind-styled-components";
+
+export const Container = tw.div`
+  flex
+  flex-col
+  items-center
+  text-gray-500
+  mt-3
+  md:mt-8
+`;
+export const Title = tw.div`
+  font-bold
+  text-xl
+`;
+export const Context = tw.div`
+  p-5
+  flex
+  flex-col
+  gap-2
+  text-justify
+  text-sm
+
+  //md
+  md:max-w-lg
+  md:text-md
+`;
+export const Interaction = tw.div`
+  font-bold
+  text-xl
+`;
+export const Icons = tw.div`
+  flex
+  items-center
+  justify-center
+  gap-5
+  mt-3
+  text-3xl
+`;
